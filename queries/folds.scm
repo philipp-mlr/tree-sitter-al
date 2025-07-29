66eb401c56dd34d24cf3ed0e1a971e21fbc15e72
Philipp Müller <hello@philipp.software>
2025-07-29 20:26:10 +0200

    fix: apply fixes for syntax

--- queries/folds.scm
+++ queries/folds.scm
@@ -1,5 +1,102 @@
-; folds.scm: Defining folds for AL language
+; Folds for object declarations
+(table_declaration body: (_) @fold)
+(tableextension_declaration body: (_) @fold)
+(page_declaration body: (_) @fold)
+(pageextension_declaration body: (_) @fold)
+(pagecustomization_declaration body: (_) @fold)
+(codeunit_declaration body: (_) @fold)
+(report_declaration body: (_) @fold)
+(reportextension_declaration body: (_) @fold)
+(query_declaration body: (_) @fold)
+(xmlport_declaration body: (_) @fold)
+(enum_declaration body: (_) @fold)
+(enumextension_declaration body: (_) @fold)
+(interface_declaration body: (_) @fold)
+(profile_declaration body: (_) @fold)
+(profileextension_declaration body: (_) @fold)
+(controladdin_declaration body: (_) @fold)
+(permissionset_declaration body: (_) @fold)
+(permissionsetextension_declaration body: (_) @fold)
+(entitlement_declaration body: (_) @fold)
+(dotnet_declaration body: (_) @fold)
 
-; Example capture for foldable nodes
-(function_declaration
-  body: (block) @fold)
+; Folds for sections within objects
+(fields body: (_) @fold)
+(keys body: (_) @fold)
+(fieldgroups_section body: (_) @fold)
+(layout_section body: (_) @fold)
+(actions_section body: (_) @fold)
+(views_section body: (_) @fold)
+(requestpage_section body: (_) @fold)
+(dataset_section body: (_) @fold)
+(labels_section body: (_) @fold)
+(rendering_section body: (_) @fold)
+(elements_section body: (_) @fold)
+(assembly_declaration body: (_) @fold)
+(xmlport_schema_element body: (_) @fold)
+
+; Folds for procedural and control blocks
+(procedure body: (code_block) @fold)
+(trigger_declaration body: (code_block) @fold)
+(onrun_trigger body: (code_block) @fold)
+(code_block) @fold
+(if_statement) @fold
+(case_statement) @fold
+(for_statement) @fold
+(while_statement) @fold
+(repeat_statement) @fold
+(with_statement) @fold
+
+; Folds for block-like structures with {}
+(field_declaration body: (_) @fold)
+(key_declaration body: (_) @fold)
+(action_declaration body: (_) @fold)
+(actionref_declaration body: (_) @fold)
+(systemaction_declaration body: (_) @fold)
+(fileuploadaction_declaration body: (_) @fold)
+(customaction_declaration body: (_) @fold)
+(action_group_section body: (_) @fold)
+(area_action_section body: (_) @fold)
+(separator_action body: (_) @fold)
+(group_section body: (_) @fold)
+(cuegroup_section body: (_) @fold)
+(grid_section body: (_) @fold)
+(repeater_section body: (_) @fold)
+(fixed_section body: (_) @fold)
+(part_section body: (_) @fold)
+(systempart_section body: (_) @fold)
+(usercontrol_section body: (_) @fold)
+(dataitem_section body: (_) @fold)
+(report_dataitem_section body: (_) @fold)
+(column_section body: (_) @fold)
+(report_column_section body: (_) @fold)
+(filter_section body: (_) @fold)
+(view_definition body: (_) @fold)
+(rendering_layout body: (_) @fold)
+(xmlport_table_element body: (_) @fold)
+(xmlport_field_attribute body: (_) @fold)
+(xmlport_text_attribute body: (_) @fold)
+(type_declaration body: (_) @fold)
+(enum_value_declaration body: (_) @fold)
+(fieldgroup_declaration body: (_) @fold)
+
+; Folds for preprocessor directives
+(preproc_region) @fold.start
+(preproc_endregion) @fold.end
+(preprocessor_file_conditional) @fold
+(preproc_conditional_object_declaration) @fold
+(preproc_conditional_codeunit_declaration) @fold
+(preproc_conditional_enum_declaration) @fold
+(preproc_conditional_procedures) @fold
+(preproc_conditional_var_sections) @fold
+(preproc_conditional_mixed_content) @fold
+(preproc_conditional_layout) @fold
+(preproc_conditional_actions) @fold
+(preproc_conditional_if_statement) @fold
+(preproc_conditional_case) @fold
+(preproc_conditional_keys) @fold
+(preproc_conditional_fields) @fold
+(preproc_conditional_permissions) @fold
+
+; Fold for multiline comments
+(multiline_comment) @fold
---
+++ queries/injections.scm
@@ -0,0 +1,6 @@
+; extends
+
+((type_declaration
+  (dotnet_type_name) @injection.content)
+  (#set! injection.language "c_sharp"))
+
